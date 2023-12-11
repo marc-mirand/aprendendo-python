987e5aefeb36829bc18c509ba6ef274a5b2212f7
Marcos Vinícios Batista Miranda <150370240+marc-mirand@users.noreply.github.com>
2023-12-11 18:32:59 -0300

    Update des011.py

--- curso-em-video/desafios/des011.py
+++ curso-em-video/desafios/des011.py
@@ -2,4 +2,4 @@ h = float(input('Digite a altura da parede em metros:'))
 w = float(input('Digite a largura da parede em metros:'))
 a = h * w
 q = a / 2
-print('A área da parede equivale a {}m² e você precisará de {}l de tinta para pintá-la.' .format(a, q))
+print('A área da parede equivale a {:.2f}m² e você precisará de {:.2f}l de tinta para pintá-la.' .format(a, q))
