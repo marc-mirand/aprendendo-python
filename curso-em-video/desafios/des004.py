ae27d4bc5873c49503b8efd3126ed510c8039364
Marcos Vinícios Batista Miranda <150370240+marc-mirand@users.noreply.github.com>
2023-12-11 11:47:45 -0300

    Add files via upload

---
+++ curso-em-video/desafios/des004.py
@@ -0,0 +1,10 @@
+x = input('Digite algo:')
+print('O tipo primitivo desse valor é: {}' .format(type(x)))
+print('Só tem espaços?', x.isspace())
+print('É um número?', x.isnumeric())
+print('É alfabético?', x.isalpha())
+print('É alfanumérico?', x.isalnum())
+print('Está em maiúsculas?', x.isupper())
+print('Esta em minúsculas?', x.islower())
+print('É capitalizada?', x.istitle())
+
