56cc39a8f4866d3c7e4e53acb95ceca0a6407783
Marcos Vinícios Batista Miranda <150370240+marc-mirand@users.noreply.github.com>
2023-12-11 18:35:04 -0300

    Update des012.py

--- curso-em-video/desafios/des012.py
+++ curso-em-video/desafios/des012.py
@@ -1,3 +1,3 @@
 p = float(input('Digite o valor atual no produto:'))
 n = p - (p * 0.05)
-print('O valor do produto com desconto é: {}R$.' .format(n))
+print('O valor do produto com desconto é: {:.2f}R$.' .format(n))
