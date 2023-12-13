92e7bf7c6db31726fee5ecd1a5c623ca8b504b69
Marcos Vinícios Batista Miranda <150370240+marc-mirand@users.noreply.github.com>
2023-12-13 18:19:49 -0300

    Update des016.py

--- curso-em-video/desafios/des016.py
+++ curso-em-video/desafios/des016.py
@@ -1,5 +1,4 @@
 #Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
 from math import trunc
 num = float(input('Digite um número real:'))
-inte = trunc(num)
-print('O número {} tem a parte inteira {}' .format(num, inte))
+print('O número {} tem a parte inteira {}' .format(num, trunc(num)))
