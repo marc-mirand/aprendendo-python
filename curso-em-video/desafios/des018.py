aca03d60f30d99cfc9c248bf8aab7ac0fa10600c
Marcos Vinícios Batista Miranda <150370240+marc-mirand@users.noreply.github.com>
2023-12-13 18:40:14 -0300

    Update des018.py

--- curso-em-video/desafios/des018.py
+++ curso-em-video/desafios/des018.py
@@ -1,4 +1,4 @@
 #Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangete desse ângulo.
 from math import cos, sin, tan
 ang = float(input('Insira o valor do ângulo:'))
-print('Para o ângulo {} temos:/n Seno: {}/n Cosseno: {}/n Tangente: {}' .format(ang, sin(ang), cos(ang), tan(ang)))
+print('Para o ângulo {:.1f} temos:\n Seno: {:.2f}\n Cosseno: {:.2f}\n Tangente: {:.2f}' .format(ang, sin(ang), cos(ang), tan(ang)))
